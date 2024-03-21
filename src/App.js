d9c068d818cf95c206418f3dbf1343f6ce184bfe
Sandeep Bhamdare <sandeepbhamdare3161@gmail.com>
2024-03-21 18:54:03 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom"
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Cart from './page/Cart';
@@ -11,11 +12,13 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* <Home /> */}
-      {/* <Login /> */}
-      {/* <Signup/> */}
-      {/* <Wishlist /> */}
-      <Cart/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
       <Footer />
     </>
   );
